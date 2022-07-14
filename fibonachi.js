13a1301c2697ecaf405c51de1c57d5e41687fd69
Nastya880 <56202516+Nastya880@users.noreply.github.com>
2022-07-14 19:23:00 +0300

    day00, ex03

---
+++ fibonachi.js
@@ -0,0 +1,13 @@
+// Напишите функицю, которая принимает индекс числа из ряда Фибоначчи и возвращает его значение
+// Предположим, что ряд Фибоначчи начинается с 0 индекса
+
+function fibo(index) {
+    if (index == 0 || index == 1) return 1;
+    else return fibo(index - 1) + fibo(index - 2);
+  }
+  
+console.log(fibo(6)); // Вернет 13
+console.log(fibo(5)); // Вернет 8
+console.log(fibo(1)); // Вернет 1
+console.log(fibo(8)); // Вернет 34
+console.log(fibo(21)); // Вернет 17711
