04ec7e3850720d30ffa0b74b3dd9827d60a924d1
Nastya880 <56202516+Nastya880@users.noreply.github.com>
2022-07-15 15:19:22 +0300

    day00, ex07 done. But reverse order

--- atm.js
+++ atm.js
@@ -7,8 +7,7 @@ function atm(sum) {
   const banknots = [5000, 2000, 1000, 500, 200, 100, 50];
   const result = [];
 
-  if (sum % 50 != 0)
-    return ("Incorrect value");
+  if (sum % 50 !== 0) return "Incorrect value";
   if (sum > 0) {
     for (let i = 0; i < banknots.length; i++) {
       let note = banknots[i];
@@ -19,7 +18,18 @@ function atm(sum) {
       }
     }
   }
-  return result;
+  if (result.length > 20) return "limit exceeded";
+  let map_res = {};
+  let q = 1;
+  for (let i = 0; i < result.length; i++) {
+    if (result[i] !== result[i + 1]) {
+      map_res[result[i]] = q;
+      q = 1;
+    } else {
+      q++;
+    }
+  }
+  return map_res;
 }
 
 console.log(atm(8350)); // {5000 : 1, 2000 : 1, 1000 : 1, 200 : 1, 100 : 1, 50 : 1 }
