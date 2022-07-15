9046d5037b17256cec2a20e1fccedae98df68d60
Nastya880 <56202516+Nastya880@users.noreply.github.com>
2022-07-15 10:51:17 +0300

    day00, ex02 done ?

--- paskalsTriangle.js
+++ paskalsTriangle.js
@@ -3,9 +3,14 @@
 // https://cdn.fishki.net/upload/post/201502/04/1414683/947eb978f710426fd0702fd119da506b.gif тут можно посмотреть наглядно принцип работы
 // Считай, что начальные координаты 0,0
 // Тут, возможно, поможет рекурсия
-
+// x - номер позиции, у - номер строки
 function paskalsTriangle(x, y) {
-
+    if (x === 0 || y === x) {
+        return 1;
+    }
+    return paskalsTriangle(x - 1, y - 1) + paskalsTriangle(x, y - 1);
 }
 
-paskalsTriangle(3, 2) // 3
+console.log(paskalsTriangle(2, 3)); // 3
+console.log(paskalsTriangle(4, 5)); // 5
+console.log(paskalsTriangle(1, 1)); // 1
