56f6591879ceb46cd5da4314c3ff5fa6c13e56d5
Nastya880 <56202516+Nastya880@users.noreply.github.com>
2022-07-15 17:52:06 +0300

    day01, ex01

---
+++ classes.js
@@ -0,0 +1,41 @@
+/*
+
+У экземпляра класса должны присутствовать св-ва:
+-name string
+-grade string Для простоты предположим, что система грейдов будет иметь значения от L1 до L4
+-hardSkills string[]
+-company string
+
+
+Так же должны иметься три метода:
+
+-changeCompany(newCompanyName) - сотрудник может сменить компанию, либо же просто уволиться
+-upGrade(newGradeName) - сотрудник может повысить квалификацию
+-addSkill(newSkillName) - сотрудник может дополнить список своих скиллов.
+*/
+
+
+export class Empleyee {
+    constructor (name, grade, hardSkills, company) {
+        this.name = name;
+        this.grade = grade;
+        this.hardSkills = hardSkills;
+        this.company = company;
+    }
+
+    changeCompany(newCompanyName) {
+        this.company = newCompanyName;
+    };
+    upGrade(newGradeName) {
+        this.grade = newGradeName
+    };
+    addSkill(newSkillName) {
+        this.hardSkills.push(newSkillName);
+    };
+    //toString () {
+      //  return `${this.day}/${this.month}/${this.year}`;
+    //}
+
+}
+
+let worker = new Empleyee("User1", "L2", "informatics", "www")
