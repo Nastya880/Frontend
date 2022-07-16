993f7d2878044db8e9f2c3fa39a4116441302ada
Nastya880 <56202516+Nastya880@users.noreply.github.com>
2022-07-16 09:51:04 +0300

    day00, add correct processing error

--- fibonachi.js
+++ fibonachi.js
@@ -2,12 +2,15 @@
 // Предположим, что ряд Фибоначчи начинается с 0 индекса
 
 function fibo(index) {
-    if (index === 0 || index === 1) return 1;
-    else return fibo(index - 1) + fibo(index - 2);
-  }
-  
+  if (index < 0) return "Error. Please input correct number!";
+  if (index === 0 || index === 1) return 1;
+  else return fibo(index - 1) + fibo(index - 2);
+}
+
 console.log(fibo(6)); // Вернет 13
 console.log(fibo(5)); // Вернет 8
 console.log(fibo(1)); // Вернет 1
 console.log(fibo(8)); // Вернет 34
 console.log(fibo(21)); // Вернет 17711
+console.log(fibo(0));
+console.log(fibo(-1));
