6a607a84fbef1d8b3ba38bd8bde59fc90548e655
Nastya880 <56202516+Nastya880@users.noreply.github.com>
2022-07-16 09:59:33 +0300

    day00, add processing errors

--- paskalsTriangle.js
+++ paskalsTriangle.js
@@ -5,12 +5,15 @@
 // Тут, возможно, поможет рекурсия
 // x - номер позиции, у - номер строки
 function paskalsTriangle(x, y) {
-    if (x === 0 || y === x) {
-        return 1;
-    }
-    return paskalsTriangle(x - 1, y - 1) + paskalsTriangle(x, y - 1);
+  if (x < 0 || y < 0) return "Error. Please input positive coordinats\n";
+  if (x === 0 || y === x) {
+    return 1;
+  }
+  if (x < 0 || y - 1 < 0) return "Error\n";
+  else return paskalsTriangle(x - 1, y - 1) + paskalsTriangle(x, y - 1);
 }
 
 console.log(paskalsTriangle(2, 3)); // 3
 console.log(paskalsTriangle(4, 5)); // 5
 console.log(paskalsTriangle(1, 1)); // 1
+console.log(paskalsTriangle(1, -1)); // error
